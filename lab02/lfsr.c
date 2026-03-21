8e20496272b44937d59a6f73c550b1434e8a9f53
JJ <J@local.machine>
2026-03-21 21:26:59 +0100

    finished lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,14 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+	uint16_t mask = 0;
+	uint16_t copy = *reg;
+	mask = mask ^ copy;
+	mask = mask ^ (copy >> 2);
+	mask = mask ^ (copy >> 3);
+	mask = mask ^ (copy >> 5);
+	mask = mask & 1;
+	*reg = (*reg >> 1) | (mask << 15);
 }
+
 
